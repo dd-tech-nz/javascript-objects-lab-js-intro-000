4987b8985c1b933d5dc216898e8a40f4fbdad783
David Dumolo <ddumolo@hotmail.com>
2020-03-11 06:20:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var recipes = {eggs: '3'}
 
 function updateObjectWithKeyAndValue(object, key, value) {
   var newObj = Object.assign({object}, {[key]: value})
-  return object, newObj
+  return newObj
 }
 
