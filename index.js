0104c6c0a49a52f392890dd0f71170c6a301774e
David Dumolo <ddumolo@hotmail.com>
2020-03-11 06:40:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,4 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign(object)
-}
+
